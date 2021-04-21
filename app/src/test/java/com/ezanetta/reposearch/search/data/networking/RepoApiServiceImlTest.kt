b633b9fc000671d22074db8877c0834bca1c6afb
Ezequiel <ezequielzanetta@gmail.com>
2021-04-21 19:44:34 -0300

    Add unit tests for networking and usecase layers

---
+++ app/src/test/java/com/ezanetta/reposearch/search/data/networking/RepoApiServiceImlTest.kt
@@ -0,0 +1,63 @@
+package com.ezanetta.reposearch.search.data.networking
+
+import com.ezanetta.reposearch.search.data.model.Owner
+import com.ezanetta.reposearch.search.data.model.RepoItem
+import com.ezanetta.reposearch.search.data.model.Result
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+@ExperimentalCoroutinesApi
+class RepoApiServiceImlTest {
+
+    private val apiClient: RepoApiClient = mockk()
+    private val repoApiServiceImpl = RepoApiServiceImpl(apiClient)
+
+    @Test
+    fun `getReposByQuery SHOULD return a list of RepoItem WHEN response is Success`() {
+        runBlocking {
+            // GIVEN
+            coEvery {
+                apiClient.searchReposByUsername(any())
+            } returns repoList
+
+            // WHEN
+            val response: Flow<Result<List<RepoItem>>> =
+                repoApiServiceImpl.getReposByQuery(USERNAME, 0)
+
+            // THEN
+            response.collect { result ->
+                assertTrue(result is Result.Success)
+            }
+        }
+    }
+
+    @Test
+    fun `getReposByQuery SHOULD return a Failure response when an error occurs`() {
+        runBlocking {
+            // GIVEN
+            coEvery {
+                apiClient.searchReposByUsername(any())
+            } throws Exception()
+
+            // WHEN
+            val response: Flow<Result<List<RepoItem>>> =
+                repoApiServiceImpl.getReposByQuery(USERNAME, 0)
+
+            // THEN
+            response.collect {
+                assertTrue(it is Result.Failure)
+            }
+        }
+    }
+
+    private companion object {
+        const val USERNAME = "Google"
+        val repoList = listOf(RepoItem("Fake repo", Owner("testUrl")))
+    }
+}
